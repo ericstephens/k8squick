40f619982a56199312897f84ff81cdb84abcc6ec
Eric A. Stephens <e.a.stephens@me.com>
2020-12-01 18:01:00 -0500

    got a simple api working on the local k8s cluster

--- metricfire/app/app.py
+++ metricfire/app/app.py
@@ -1,9 +1,10 @@
+#https://www.metricfire.com/blog/develop-and-deploy-a-python-api-with-kubernetes-and-docker-part-ii/
+
 from flask import Flask
 import requests
+import os
 
 app = Flask(__name__)
-
-API_KEY = "47528ff57f24344dd3b93b4564e9df1e"
 
 @app.route('/')
 def index():
@@ -11,26 +12,7 @@ def index():
 
 @app.route('/reverse/<string:str>')
 def reverse(str):
-    data = str[::-1]
-    return data
-
-@app.route('/<string:city>')
-def weather_by_city(city):
-
-# https://samples.openweathermap.org/data/2.5/weather?
-
-    #test =  "https://api.openweathermap.org/data/2.5/weather?q=London&appid=b43cb98f3b3d2006fcfb8524bb058ba9"
-
-    url = 'https://samples.openweathermap.org/data/2.5/weather'
-    params = dict(
-        q=city,
-        appid=API_KEY,
-    )
-
-    print(params)
-
-    response = requests.get(url=url, params=params)
-    data = response.json()
+    data = str + ":" + str[::-1]
     return data
 
 if __name__ == '__main__':
